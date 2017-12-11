afb56f8f84ef01ae60ccbd9922bf76afaf20d8f0
Rasmus Handberg <rasmush@phys.au.dk>
2017-12-11 16:43:40 +0100

    Improvements to documentation of backgrounds

--- photometry/backgrounds.py
+++ photometry/backgrounds.py
@@ -1,20 +1,40 @@
-#!/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Estimation of sky background in TESS Full Frame Images.
+"""
+
 from __future__ import division, with_statement, print_function, absolute_import
+import six
 import numpy as np
 from astropy.io import fits
 from astropy.stats import SigmaClip
 from photutils import Background2D, SExtractorBackground
 
-def fit_background(fname):
+def fit_background(image):
+	"""
+	Estimate background in Full Frame Image.
+
+	Parameters:
+		image (ndarray or string): Either the image as 2D ndarray or a path to FITS or NPY file where to load image from.
+
+	Returns:
+		ndarray: Estimated background with the same size as the input image.
+		ndarray: Mask specifying which pixels was used to estimate the background.
+	"""
 
 	# Load file:
-	if fname.endswith('.npy'):
-		img = np.load(fname)
+	if isinstance(image, np.ndarray):
+		img = image
+	elif isinstance(image, six.string_types):
+		if image.endswith('.npy'):
+			img = np.load(image)
+		else:
+			with fits.open(image, memmap=True, mode='readonly') as hdu:
+				img = hdu[0].data
 	else:
-		with fits.open(fname, memmap=True, mode='readonly') as hdu:
-			img = hdu[0].data
+		raise ValueError("Input image must be either 2D ndarray or path to file.")
 
 	# Create mask
 	# TODO: Use the known locations of bright stars
@@ -29,5 +49,5 @@ def fit_background(fname):
 		sigma_clip=sigma_clip,
 		bkg_estimator=bkg_estimator,
 		mask=mask)
-		
+
 	return bkg.background, mask
