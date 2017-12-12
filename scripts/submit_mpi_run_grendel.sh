57311a04d9d161c5d295f5c146028543b1f21103
Rasmus Handberg <rasmush@phys.au.dk>
2017-12-12 16:38:40 +0100

    Minor changes to MPI submit script

--- scripts/submit_mpi_run_grendel.sh
+++ scripts/submit_mpi_run_grendel.sh
@@ -6,9 +6,10 @@
 #SBATCH --ntasks-per-node=20
 #SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
-#SBATCH --time=6:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rasmush@phys.au.dk
+#SBATCH --output=/home/rasmush/tasoc/output-slurm/slurm-%j.out
 
 echo "========= Job started  at `date` =========="
 
@@ -25,17 +26,16 @@ export TESSPHOT_INPUT="/scratch/astro/tess/input"
 export TESSPHOT_OUTPUT="/scratch/astro/tess/output"
 
 # Move the program to the scratch disk:
+rsync -a --delete ~/tasoc/input/ /scratch/astro/tess/input/
 rsync -a --delete ~/tasoc/photometry/ /scratch/astro/tess/program/
 
 # Change directory to the local scratch-directory:
 cd /scratch/astro/tess/program
 
 # Run the MPI job:
-mpiexec python mpi_scheduler.py > out.txt 2>&1
+mpiexec python mpi_scheduler.py
 
 # Copy some of the output to the home directory:
-mv out.txt ~/tasoc/output-slurm/
-mv slurm-*.out ~/tasoc/output-slurm/
 cp /scratch/astro/tess/input/todo.sqlite ~/tasoc/output-slurm/
 mv /scratch/astro/tess/output/* ~/tasoc/output-fits/
 
