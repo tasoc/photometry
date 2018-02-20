3195becff018ce3b5ae30afb654d1128dd154c0c
Rasmus Handberg <rasmush@phys.au.dk>
2018-02-20 22:16:32 +0100

    WIP: Added test of ImageMovementKernel

---
+++ tests/test_imagemotion.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. codeauthor:: Rasmus Handberg <rasmush@phys.au.dk>
+"""
+
+from __future__ import division, print_function, with_statement, absolute_import
+import sys
+import os.path
+import glob
+import numpy as np
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from photometry.image_motion import ImageMovementKernel
+from photometry.utilities import load_ffi_fits
+
+def test_imagemotion():
+	"""Test of ImageMovementKernel"""
+
+	# Load the first image in the input directory:
+	INPUT_DIR = os.path.join(os.path.dirname(__file__), 'input', 'images')
+	files = glob.glob(os.path.join(INPUT_DIR, '*.fits.gz'))
+	fname = sorted(files)[0]
+
+	# Load the image:
+	img = load_ffi_fits(fname)
+	
+	# Some positions across the image:
+	xx, yy = np.meshgrid(
+		np.linspace(0, img.shape[0], 5, dtype='float64'),
+		np.linspace(0, img.shape[1], 5, dtype='float64'),
+	)
+	xy = np.column_stack((xx.flatten(), yy.flatten()))
+	print("Positions to be tested:")
+	print(xy)
+	
+	for warpmode in ('translation', 'euclidian'):
+		print("Testing warpmode=" + warpmode)
+	
+		# Create ImageMovementKernel instance:
+		imk = ImageMovementKernel(image_ref=img, warpmode=warpmode)
+		
+		# Calculate kernel for the same image:
+		kernel = imk.calc_kernel(img)
+		print("Kernel:")
+		print(kernel)
+		assert(len(kernel) == imk.n_params)
+		
+		# Calculate the new positions based on the kernel:
+		delta_pos = imk(xy, kernel)
+		print("Extracted movements:")
+		print(delta_pos)
+		
+		assert(delta_pos.shape == xy.shape)
+		
+		# The movements should all be very close to zero,
+		# since we used the same image as the reference:
+		np.testing.assert_allclose(delta_pos, np.zeros_like(xy), atol=1e-7)
+		
+	print("Done")
+
+if __name__ == '__main__':
+	test_imagemotion()
