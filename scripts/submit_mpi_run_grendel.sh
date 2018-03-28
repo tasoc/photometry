6adf2b5cd4d809c4f24c617dc0f7a6eac5106552
Rasmus Handberg <rasmush@phys.au.dk>
2018-03-28 22:12:32 +0200

    WIP: Scripts for Grendel supercomputer

--- scripts/submit_mpi_run_grendel.sh
+++ scripts/submit_mpi_run_grendel.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=tessphot
-#SBATCH --partition=q20
+#SBATCH --partition=q36
 #SBATCH --constraint=astro
-#SBATCH --ntasks=20
-#SBATCH --ntasks-per-node=20
+#SBATCH --ntasks=36
+#SBATCH --ntasks-per-node=36
 #SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
 #SBATCH --time=48:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rasmush@phys.au.dk
-#SBATCH --output=/home/rasmush/tasoc/output-slurm/slurm-%j.out
+#SBATCH --output=/home/rasmush/tasoc/slurm-%j-tessphot.out
 
 echo "========= Job started  at `date` =========="
 
@@ -19,6 +19,7 @@ module load intel
 module load openmpi
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
 source ~/python-virtualenv/tessphot/bin/activate
+echo "Using $OMP_NUM_THREADS CPUs"
 
 # Set environment variables with paths for input and
 # output files for the photometry code:
@@ -26,7 +27,6 @@ export TESSPHOT_INPUT="/scratch/astro/tess/input"
 export TESSPHOT_OUTPUT="/scratch/astro/tess/output"
 
 # Move the program to the scratch disk:
-rsync -a --delete ~/tasoc/input/ /scratch/astro/tess/input/
 rsync -a --delete ~/tasoc/photometry/ /scratch/astro/tess/program/
 
 # Change directory to the local scratch-directory:
@@ -37,7 +37,6 @@ mpiexec python mpi_scheduler.py
 
 # Copy some of the output to the home directory:
 cp /scratch/astro/tess/input/todo.sqlite ~/tasoc/output-slurm/
-mv /scratch/astro/tess/output/* ~/tasoc/output-fits/
 
 echo "========= Job finished at `date` =========="
 #
--- scripts/submit_prepare_grendel.sh
+++ scripts/submit_prepare_grendel.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=tessphot
-#SBATCH --partition=q20
+#SBATCH --job-name=prepare
+#SBATCH --partition=q36
 #SBATCH --constraint=astro
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=36
 #SBATCH --mem=0
 #SBATCH --export=NONE
-#SBATCH --time=12:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rasmush@phys.au.dk
+#SBATCH --output=/home/rasmush/tasoc/slurm-%j-prepare.out
 
 echo "========= Job started  at `date` =========="
 
 # Load required modules and avtivate the virtualenv:
 source /comm/swstack/bin/modules.sh --silent
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
-echo "Using {$OMP_NUM_THREADS} CPUs"
+echo "Using $OMP_NUM_THREADS CPUs"
 source ~/python-virtualenv/tessphot/bin/activate
 
 # Set environment variables with paths for input and
@@ -27,21 +28,18 @@ export TESSPHOT_OUTPUT="/scratch/astro/tess/output"
 # Move the program to the scratch disk:
 rsync -a --delete ~/tasoc/photometry/ /scratch/astro/tess/program/
 
-rsync -a --stats ~/tasoc/input/ /scratch/astro/tess/input/
-
 # Change directory to the local scratch-directory:
 cd /scratch/astro/tess/program
 
 # Run the MPI job:
 echo "Running prepare..."
-python prepare_photometry.py --debug 14
+python prepare_photometry.py
 
 echo "Running make_todo..."
 python make_todo.py
 
 # Copy some of the output to the home directory:
-#mv prepare-out.txt ~/tasoc/output-slurm/
-#mv slurm-*.out ~/tasoc/output-slurm/
+cp /scratch/astro/tess/input/todo.sqlite ~/tasoc/input/
 
 echo "========= Job finished at `date` =========="
 #
