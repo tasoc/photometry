3f5b52f4aeb09a0cb42ccb06b7a2ca6acbc41326
Benjamin Pope <benjamin.pope@nyu.edu>
2018-07-17 10:22:54 +0200

    spaces

--- photometry/halo/halo_photometry.py
+++ photometry/halo/halo_photometry.py
@@ -51,86 +51,86 @@ class HaloPhotometry(BasePhotometry):
 		cols, rows = self.get_pixel_grid()
 
 		'''
-        Parameters
-        ----------
-        aperture_mask : array-like, 'pipeline', or 'all'
-            A boolean array describing the aperture such that `False` means
-            that the pixel will be masked out.
-            If the string 'all' is passed, all pixels will be used.
-            The default behaviour is to use the Kepler pipeline mask.
-        splits : tuple, (None, None) or (2152,2175) etc.
-            A tuple including two times at which to split the light curve and run halo 
-            separately outside these splits.
-        sub : int
-            Do you want to subsample every nth pixel in your light curve? Not advised, 
-            but can come in handy for very large TPFs.
-        order: int
-            Run nth order TV - ie first order is L1 norm on first derivative,
-            second order is L1 norm on second derivative, etc.
-            This is part of the Pock generalized TV scheme, so that
-            1st order gives you piecewise constant functions,
-            2nd order gives you piecewise affine functions, etc. 
-            Currently implemented only up to 2nd order in numerical, 1st in analytic!
-            We recommend first order very strongly.
-        maxiter: int
-            Number of iterations to optimize. 101 is default & usually sufficient.
-        w_init: None or array-like.
-            Initialize weights with a particular weight vector - useful if you have
-            already run TV-min and want to update, but otherwise set to None 
-            and it will have default initialization.
-        random_init: Boolean
-            If False, and w_init is None, it will initialize with uniform weights; if True, it
-            will initialize with random weights. False is usually better.
-        thresh: float
-            A float greater than 0. Pixels less than this fraction of the maximum
-            flux at any pixel will be masked out - this is to deal with saturation.
-            Because halo is usually intended for saturated stars, the default is 0.8, 
-            to deal with saturated pixels. If your star is not saturated, set this 
-            greater than 1.0. 
-        consensus: Boolean
-            If True, this will subsample the pixel space, separately calculate halo time 
-            series for eah set of pixels, and merge these at the end. This is to check
-            for validation, but is typically not useful, and is by default set False.
-        analytic: Boolean
-            If True, it will optimize the TV with autograd analytic derivatives, which is
-            several orders of magnitude faster than with numerical derivatives. This is 
-            by default True but you can run it numerically with False if you prefer.
-        sigclip: Boolean
-            If True, it will iteratively run the TV-min algorithm clipping outliers.
-            Use this for data with a lot of outliers, but by default it is set False.
-        '''
+		Parameters
+		----------
+		aperture_mask : array-like, 'pipeline', or 'all'
+			A boolean array describing the aperture such that `False` means
+			that the pixel will be masked out.
+			If the string 'all' is passed, all pixels will be used.
+			The default behaviour is to use the Kepler pipeline mask.
+		splits : tuple, (None, None) or (2152,2175) etc.
+			A tuple including two times at which to split the light curve and run halo 
+			separately outside these splits.
+		sub : int
+			Do you want to subsample every nth pixel in your light curve? Not advised, 
+			but can come in handy for very large TPFs.
+		order: int
+			Run nth order TV - ie first order is L1 norm on first derivative,
+			second order is L1 norm on second derivative, etc.
+			This is part of the Pock generalized TV scheme, so that
+			1st order gives you piecewise constant functions,
+			2nd order gives you piecewise affine functions, etc. 
+			Currently implemented only up to 2nd order in numerical, 1st in analytic!
+			We recommend first order very strongly.
+		maxiter: int
+			Number of iterations to optimize. 101 is default & usually sufficient.
+		w_init: None or array-like.
+			Initialize weights with a particular weight vector - useful if you have
+			already run TV-min and want to update, but otherwise set to None 
+			and it will have default initialization.
+		random_init: Boolean
+			If False, and w_init is None, it will initialize with uniform weights; if True, it
+			will initialize with random weights. False is usually better.
+		thresh: float
+			A float greater than 0. Pixels less than this fraction of the maximum
+			flux at any pixel will be masked out - this is to deal with saturation.
+			Because halo is usually intended for saturated stars, the default is 0.8, 
+			to deal with saturated pixels. If your star is not saturated, set this 
+			greater than 1.0. 
+		consensus: Boolean
+			If True, this will subsample the pixel space, separately calculate halo time 
+			series for eah set of pixels, and merge these at the end. This is to check
+			for validation, but is typically not useful, and is by default set False.
+		analytic: Boolean
+			If True, it will optimize the TV with autograd analytic derivatives, which is
+			several orders of magnitude faster than with numerical derivatives. This is 
+			by default True but you can run it numerically with False if you prefer.
+		sigclip: Boolean
+			If True, it will iteratively run the TV-min algorithm clipping outliers.
+			Use this for data with a lot of outliers, but by default it is set False.
+		'''
 
 		splits=(None,None)
 		sub=1
 		order=1
-	    maxiter=101
-	    w_init=None
-	    random_init=False
-	    thresh=0.8
-	    minflux=100.
-	    consensus=False
-	    analytic=True
-	    sigclip=False
+		maxiter=101
+		w_init=None
+		random_init=False
+		thresh=0.8
+		minflux=100.
+		consensus=False
+		analytic=True
+		sigclip=False
 
-	    logger.info('Formatting TPF data for halo')
+		logger.info('Formatting TPF data for halo')
 		
-	    flux = self.images_cube
-	    flux[self.pixelflags==0] = np.nan
-	    time = self.lightcurve['time']
-	    quality = self.lightcurve['quality']
-	    x, y = self.tpf[1].data['POS_CORR1'], self.tpf[1].data['POS_CORR2']
-        
-        ts = Table({'time':self.time,
-                    'cadence':self.lightcurve['cadenceno'],
-                    'x':x,
-                    'y':y,
-                    'quality':quality})
+		flux = self.images_cube
+		flux[self.pixelflags==0] = np.nan
+		time = self.lightcurve['time']
+		quality = self.lightcurve['quality']
+		x, y = self.tpf[1].data['POS_CORR1'], self.tpf[1].data['POS_CORR2']
+		
+		ts = Table({'time':self.time,
+					'cadence':self.lightcurve['cadenceno'],
+					'x':x,
+					'y':y,
+					'quality':quality})
 
-        try:
+		try:
 			logger.info('Attempting TV-min photometry')
 			pf, ts, weights, weightmap, pixels_sub = do_lc(flux,
-	                    ts,splits,sub,order,maxiter=101,w_init=None,random_init=False,
-	            thresh=0.8,minflux=100.,consensus=False,analytic=True,sigclip=False)
+						ts,splits,sub,order,maxiter=101,w_init=None,random_init=False,
+				thresh=0.8,minflux=100.,consensus=False,analytic=True,sigclip=False)
 
 			self.lightcurve['corr_flux'] = ts['corr_flux']
 			self.halo_weightmap = weightmap
@@ -147,7 +147,7 @@ class HaloPhotometry(BasePhotometry):
 			cmap.set_bad('k',1.)
 			im = np.log10(weightmap.T*norm)
 			plt.imshow(im,cmap=cmap, vmin=-2*np.nanmax(im),vmax=2*np.nanmax(im),
-			    interpolation='None',origin='lower')
+				interpolation='None',origin='lower')
 			plt.colorbar()
 			plt.title('TV-min Weightmap')
 			plt.savefig('%sweightmap.png' % self.plot_folder)
