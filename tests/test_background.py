f7c9f7104434d56b0465877aa5ed1197939930fb
Rasmus Handberg <rasmush@phys.au.dk>
2019-04-19 23:39:29 +0200

    Fixed background tests
    Reverted to old versions where we didn't test for both with and without radial component

--- tests/test_background.py
+++ tests/test_background.py
@@ -21,29 +21,7 @@ def test_background():
 	img, hdr = load_ffi_fits(fname, return_header=True)
 
 	# Estimate the background:
-	bck, mask = fit_background(fname)
-
-	# Print some information:
-	print(fname)
-	print(bck.shape)
-	print(mask.shape)
-
-	# Check the sizes of the returned images:
-	assert(bck.shape == img.shape)
-	assert(mask.shape == img.shape)
-
-#------------------------------------------------------------------------------
-def test_background_with_radial():
-	"""Test of background estimator including radial component"""
-
-	# Load the first image in the input directory:
-	INPUT_DIR = os.path.join(os.path.dirname(__file__), 'input', 'images')
-	fname = find_ffi_files(INPUT_DIR)[0]
-	img, hdr = load_ffi_fits(fname, return_header=True)
-
-	# Estimate the background:
-	camera_centre = [324.566998914166, -33.172999301379]
-	bck, mask = fit_background(fname, camera_centre=camera_centre)
+	bck, mask = fit_background(fname, )
 
 	# Print some information:
 	print(fname)
@@ -57,4 +35,3 @@ def test_background_with_radial():
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
 	test_background()
-	test_background_with_radial()
