db21939203350d51f4b782c8e3da9a91a8ea16d8
Rasmus Handberg <rasmush@phys.au.dk>
2020-01-13 12:57:09 +0100

    Store final used pixels in variable to be returned later

--- photometry/halo/halo_photometry.py
+++ photometry/halo/halo_photometry.py
@@ -100,14 +100,17 @@ class HaloPhotometry(BasePhotometry):
 		logger.info('Formatting data for halo')
 		indx_goodtimes = np.isfinite(self.lightcurve['time'])
 		flux = self.images_cube.T[indx_goodtimes, :, :]
-		flux[:, self.aperture.T == 0] = np.nan
 
 		# Cut away pixels farther away than 20 pixels:
 		# TODO: We should maybe use self.resize_stamp instead, at least for FFIs.
 		# TODO: Should the limit scale with Tmag?
+		# TODO: Is there a one pixel offset in dist?
 		cols, rows = self.get_pixel_grid()
 		dist = np.sqrt((cols - self.target_pos_column)**2 + (rows - self.target_pos_row)**2)
-		flux[:, dist.T > dist_max] = np.NaN
+		pixel_mask = (self.aperture & 1 != 0) & (dist <= dist_max)
+
+		# Cut out the pixel mask in flux cube:
+		flux[:, ~pixel_mask.T] = np.NaN
 
 		# Find timestamps where the timeseries should be split:
 		if self.sector == 1:
