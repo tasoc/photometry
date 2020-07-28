4f910566c0b86cea190aaec488a01e6198b9ee50
Rasmus Handberg <rasmush@phys.au.dk>
2020-07-28 22:47:39 +0200

    Added test of make_todo.py

--- tests/test_todolist.py
+++ tests/test_todolist.py
@@ -8,15 +8,13 @@ import pytest
 import os
 import numpy as np
 import tempfile
-import logging
 import sqlite3
 import contextlib
 #import itertools
-import conftest # noqa: F401
+from conftest import capture_cli
 from photometry import todolist
-from photometry.utilities import TqdmLoggingHandler
 
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 def test_methods_file():
 
 	methods_file = os.path.join(os.path.dirname(__file__), '..', 'photometry', 'data', 'todolist-methods.dat')
@@ -31,7 +29,7 @@ def test_methods_file():
 		assert d[2] in ('ffi', 'tpf')
 		assert d[3] in ('aperture', 'halo', 'psf', 'linpsf')
 
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 def test_exclude_file():
 
 	methods_file = os.path.join(os.path.dirname(__file__), '..', 'photometry', 'data', 'todolist-exclude.dat')
@@ -45,7 +43,7 @@ def test_exclude_file():
 		assert d[1] > 0
 		assert d[2] in ('ffi', 'tpf')
 
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 #def test_calc_cbv_area():
 #
 #	for camera, ccd in itertools.product((1,2,3,4), (1,2,3,4)):
@@ -66,14 +64,14 @@ def test_exclude_file():
 #		print(cbv_area)
 #		assert(cbv_area == 131)
 
-#----------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
 def test_make_todolist(SHARED_INPUT_DIR):
 	with tempfile.NamedTemporaryFile() as tmpfile:
 		# Run make_todo and save output to temp-file:
 		todolist.make_todo(SHARED_INPUT_DIR, cameras=3, ccds=2, output_file=tmpfile.name)
 
 		tmpfile.flush()
-		assert os.path.exists(tmpfile.name + '.sqlite'), "TODO-file was not created"
+		assert os.path.isfile(tmpfile.name + '.sqlite'), "TODO-file was not created"
 
 		with contextlib.closing(sqlite3.connect("file:" + tmpfile.name + '.sqlite?mode=ro', uri=True)) as conn:
 			conn.row_factory = sqlite3.Row
@@ -90,15 +88,40 @@ def test_make_todolist(SHARED_INPUT_DIR):
 
 			cursor.close()
 
-#----------------------------------------------------------------------
-if __name__ == '__main__':
-	# Setup logging:
-	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-	console = TqdmLoggingHandler()
-	console.setFormatter(formatter)
-	logger_parent = logging.getLogger('photometry')
-	logger_parent.setLevel(logging.INFO)
-	if not logger_parent.hasHandlers():
-		logger_parent.addHandler(console)
+#--------------------------------------------------------------------------------------------------
+def test_make_todolist_cli(PRIVATE_INPUT_DIR):
 
+	# Path to the TODO-file:
+	todofile = os.path.join(PRIVATE_INPUT_DIR, 'todo.sqlite')
+
+	# Delete existing todo-file:
+	os.remove(todofile)
+
+	# Run make_todo CLI script:
+	out, err, exitcode = capture_cli('make_todo.py', params=['--camera=3', '--ccd=2', '"{0:s}"'.format(PRIVATE_INPUT_DIR)])
+
+	assert exitcode == 0
+	assert '- ERROR -' not in out
+	assert '- ERROR -' not in err
+	assert '- INFO - TODO done.' in err
+
+	assert os.path.isfile(todofile), "TODO-file was not created"
+
+	with contextlib.closing(sqlite3.connect("file:" + todofile + '?mode=ro', uri=True)) as conn:
+		conn.row_factory = sqlite3.Row
+		cursor = conn.cursor()
+
+		cursor.execute("SELECT * FROM todolist LIMIT 1;")
+		row = cursor.fetchone()
+		print(dict(row))
+		assert row['camera'] == 3, "Wrong camera returned"
+		assert row['ccd'] == 2, "Wrong CCD returned"
+
+		cursor.execute("SELECT COUNT(*) FROM todolist WHERE datasource='tpf';")
+		assert cursor.fetchone()[0] == 2, "Expected 2 TPFs in todolist"
+
+		cursor.close()
+
+#--------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
 	pytest.main([__file__])
