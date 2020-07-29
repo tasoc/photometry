064c94ea5e9f604594bf8fba4ea30e8c9d16a130
Rasmus Handberg <rasmush@phys.au.dk>
2020-07-29 21:37:39 +0200

    Added test of run_ffimovie.py

---
+++ tests/test_run_ffimovie.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Tests of FFI Movies.
+
+.. codeauthor:: Rasmus Handberg <rasmush@phys.au.dk>
+"""
+
+import pytest
+import os.path
+from conftest import capture_cli
+
+#--------------------------------------------------------------------------------------------------
+@pytest.mark.ffmpeg
+def test_run_ffimovie(SHARED_INPUT_DIR):
+
+	out, err, exitcode = capture_cli('run_ffimovie.py', params=[SHARED_INPUT_DIR])
+
+	assert exitcode == 0
+
+	for fname in (
+		'sector001_camera3_ccd2.mp4',
+		'sector001_combined_backgrounds.mp4',
+		'sector001_combined_flags.mp4',
+		'sector001_combined_images.mp4',
+		'sector001_combined_originals.mp4'):
+
+		mp4file = os.path.join(SHARED_INPUT_DIR, fname)
+		assert os.path.isfile(mp4file), "MP4 was not created: " + fname
+
+#--------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+	pytest.main([__file__])
